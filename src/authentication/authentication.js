7e95ebd096fc9c8df6d9d0e2a97db2337819c043
Solomon Ogunnsola <solomon.eniola7@gmail.com>
2024-05-26 15:58:43 +0100

    another update

--- src/authentication/authentication.js
+++ src/authentication/authentication.js
@@ -1,24 +1,29 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { createToken } from "../controllers/userController.js";
-import user from "../database/schema/UserSchema.js";
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
+
 const requireAuth = (req, res, next) => {
-  const token = createToken(user._id);
-  if (token) {
-    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(decodedToken);
-        next();
-      }
-    });
-  } else {
-    res.status(400).send("Not Authorized");
-    console.log();
+  // const token = createToken(user._id);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
+  // console.log("Authorization", authorization);
+
+  const bearerToken = authorization.split(" ");
+  if (bearerToken.length !== 2) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  // console.log("Token", bearerToken[1]);
+  jwt.verify(bearerToken[1], JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(decodedToken);
+      next();
+    }
+  });
 };
 
 export default requireAuth;
